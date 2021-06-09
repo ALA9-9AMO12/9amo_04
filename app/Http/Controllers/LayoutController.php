ec720d855d335a5d457f3c0501dcda7f8e109db2
esmeralddxx <e.spaander97@gmail.com>
2021-06-09 13:13:36 +0200

    Controller aangemaakt

---
+++ app/Http/Controllers/LayoutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LayoutController extends Controller
+{
+     Public function layout()
+     {
+         return view(view:'layout');
+     }
+    //
+}
