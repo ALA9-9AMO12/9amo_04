f3d2931386027f3993af7448043b9d19d6befa1f
pariyaard <1024342@mborijnland.nl>
2021-06-14 16:14:30 +0200

    changed menu nav

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,20 +1,24 @@
+<div class="topnav">
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#!">Navbar</a>
-
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav mr-auto mt-2 mt-md-0">
-            <li class="nav-item active">
-                <a class="nav-link" href="#!">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#!">Link</a>
-            </li>
-        </ul>
+    <!-- Centered link -->
+    <div class="topnav-centered">
+        <a href="#home" class="active"><img src="/img/imgpsh_mobile_save.jpg" alt="imgpsh_mobile_save" width="78" height="48"></a>
     </div>
-</nav>
+
+    <!-- Left-aligned links (default) -->
+    <a href="#">Home</a>
+    <a href="#">Artikelen</a>
+    <a href="#">Cursusen</a>
+    <a href="#">Informatie</a>
+
+    <!-- Right-aligned links -->
+    <div class="topnav-right">
+        <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a>
+        <a href="#"><i class="fa fa-fw fa-search"></i>Search</a>
+        <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
+
+    </div>
 
 
+
+</div>
