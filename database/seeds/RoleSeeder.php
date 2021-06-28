a34132848b62964417519794a0e8f7533fdcbbc6
Emrah Ertan <61698580+emrah-er@users.noreply.github.com>
2021-06-28 10:28:40 +0200

    Create seeders
    Create Role seeder.

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        DB::table('roles')->insert([
+//            ['name' => 'User'],
+//            ['name' => 'Admin'],
+//            ['name' => 'Head Admin']
+//        ]);
+
+        Role::create(['name' => 'User']);
+        Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'Head Admin']);
+    }
+}
