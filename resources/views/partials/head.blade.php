a16b1935de1532141dee2bf40af7f9aee2c722eb
Emrah Ertan <61698580+emrah-er@users.noreply.github.com>
2021-06-29 12:02:09 +0200

    Add custom css

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,8 +10,8 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link type="text/css" href="{{ asset('css/global.css') }}" rel="stylesheet">
+    <link type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
     <title>{{ config('app.name', 'Imkervereniging Oegstgeest') }}</title>
 </head>
